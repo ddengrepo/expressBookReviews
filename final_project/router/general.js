8bcf259de0d2e4e02c34d0ca55e36f8febaab688
ddengepo <ddengepo@gmail.com>
2025-04-01 18:13:52 -0400

    task 2 and 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const { JsonWebTokenError } = require('jsonwebtoken');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -17,20 +18,27 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.send(JSON.stringify(books[isbn], null, 4));
+  } else {
+    return res.status(404).json({ message: "Book not found." });
+  }
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
-});
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(book => book.author === author);
 
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const enumeratedBooks = booksByAuthor.map((book, index) => {
+    return {
+      id: index + 1, // Start index from 1
+      ...book, // Spread the original book properties
+    };
+  });
+
+  return res.send(JSON.stringify(enumeratedBooks, null, 4));
 });
 
 //  Get book review
