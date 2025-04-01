d7e91b64dec8ec2e2b99ee731f0e147eb1f0bf50
ddengepo <ddengepo@gmail.com>
2025-04-01 19:02:44 -0400

    task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,40 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+  let userwithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  // Return true if any user with the same username is found, otherwise, false
+  if (userwithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// Register a new user
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+    // Check if the user does not already exist
+    if (!doesExist(username)) {
+      users.push({
+        "username": username,
+        "password": password
+      });
+      return res.status(200).json({ message: 'User successfully registered. Now you can login' })
+    } else {
+      return res.status(404).json({ message: 'User already exists' })
+    }
+  } else {
+    return res.status(404).json({ message: 'Please provide both username and password' })
+  }
 });
 
 // Get the book list available in the shop
